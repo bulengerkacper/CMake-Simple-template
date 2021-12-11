735062ffeee8bacc3ead8c81c9128b4019da7491
bulengerkacper <bulenger.kacper@gmail.com>
2021-12-12 00:04:29 +0100

    Sample project template

---
+++ src/headers/AppConfiguration.h
@@ -0,0 +1,12 @@
+#ifndef AppConfiguration_h_
+#define AppConfiguration_h_
+
+#include <iostream>
+
+#define APP_NAME "Sample App"
+
+//
+class SampleException : public std::exception {
+};
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "AppConfiguration.h"
+
+int main() {
+
+}
